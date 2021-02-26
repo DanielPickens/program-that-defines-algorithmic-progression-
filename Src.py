5988460903cd6f63d6f17467c7d20a9538deb021
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-02-26 05:07:40 -0700

    Create Src.py

---
+++ Src.py
@@ -0,0 +1,39 @@
+def checkIsAP(arr, n): 
+
+    if (n == 1): return True
+
+  
+
+    # Sort array 
+
+    arr.sort() 
+
+  
+
+    # After sorting, difference between 
+
+    # consecutive elements must be same. 
+
+    diffInElem = arr[1] - arr[0] 
+
+    for i in range(2, n): 
+
+        if (arr[i] - arr[i-1] != diffInElem): 
+
+            return False
+
+  
+
+    return True
+
+  
+# Driver code 
+
+arr = [180, 30, 180, 80, 130, 130] 
+
+n = len(arr) 
+
+print("Yes") if(checkIsAP(arr, n)) else print("No") 
+
+  
+# This code is contributed by Anant Agarwal. 
